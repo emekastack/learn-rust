4077c8526e4bb4f6311f34c81d4d4d7af1da9681
Emeka Rex Chukwu <emekarexchukwu@gmail.com>
2025-08-08 13:47:56 +0000

    feature: add function

---
+++ function/src/main.rs
@@ -0,0 +1,50 @@
+fn main() {
+    //     let mut counter = 0;
+
+    //    let result = loop {
+    //        counter += 1;
+
+    //        if counter == 10 {
+    //         break counter * 2;
+    //        }
+    //    };
+
+    //    println!("This is the result for the loop: {result}");
+
+    // Loop Tag
+    // let mut count = 0;
+    // 'counting_up: loop {
+    //     println!("Count = {count}");
+    //     let mut remaining = 10;
+
+    //     loop {
+    //         println!("Remaining = {remaining}");
+
+    //         if remaining == 9 {
+    //             break;
+    //         }
+    //         if count == 2 {
+    //             break 'counting_up;
+    //         }
+
+    //         remaining -= 1;
+    //     }
+    //     count += 1;
+    // }
+    // println!("End count = {count}");
+
+    // While Loop
+    // let a = [10, 20, 30, 40, 50];
+    // let mut index = 0;
+
+    // while index < 5 {
+    //     println!("the value: {}", a[index]);
+
+    //     index += 1;
+    // }
+
+    // For loop
+    for element in 1..4 {
+        print!("{element} ");
+    }
+}
